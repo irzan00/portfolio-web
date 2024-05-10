ee768e247666a8fc1c3ca5056edbb9ad541349ce
IRZAN-PC\hp <irzanraka@gmail.com>
2024-05-10 11:27:08 +0700

    install bootstrap

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,7 +4,7 @@ class LandingPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>Landing Page</h1>
+                <h1 className='text-danger'>Landing Page</h1>
             </div>
         )
     }
